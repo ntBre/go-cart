eb6e45e205de1f207e4928037911de52977b4842
Brent Westbrook <brentrwestbrook@gmail.com>
2020-07-02 09:40:39 -0500

    Remove channel close on HandleSignal, caused panic

--- main.go
+++ main.go
@@ -235,11 +235,6 @@ func Index4(x, y, z, w int) int {
 // for debugging purposes to differentiate the two
 func HandleSignal(sig int, timeout time.Duration) error {
 	sigChan := make(chan os.Signal, 1)
-	// trying to stop goroutine leak
-	// stop receiving signals to send on channel
-	// then close the channel
-	defer signal.Stop(sigChan)
-	defer close(sigChan)
 	sig1Want := os.Signal(syscall.Signal(sig))
 	signal.Notify(sigChan, sig1Want)
 	select {
